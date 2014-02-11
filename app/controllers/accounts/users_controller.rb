5472d5c737e72f27ec994c55a6eef5167af6706b
Jeremy Ward <jrmy.ward@gmail.com>
2014-02-10 20:16:34 -0800

    Updated Accounts::Users controller to be more individual-centric.

--- app/controllers/accounts/users_controller.rb
+++ app/controllers/accounts/users_controller.rb
@@ -1,90 +1,33 @@
 class Accounts::UsersController < AccountsController
-  before_action :verify_deletability, only: [:destroy]
-  before_action :verify_editability, only: [:edit, :update]
-  before_action :verify_lodge_ability, only: [:create, :new]
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update]
 
   respond_to :html, :json
 
-  # GET /u/dashboard
-  def dashboard
-
-  end
-
-  # GET /:sub_domain/users
-  # GET /:sub_domain/users.json
-  def index
-    @users = current_lodge.members.order("last_name asc")
-  end
-
-  # GET /:sub_domain/users/1
-  # GET /:sub_domain/users/1.json
+  # GET /u/profile/1
+  # GET /u/profile/1.json
   def show
   end
 
-  # GET /:sub_domain/users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /:sub_domain/users/1/edit
+  # GET /u/profile/edit
   def edit
   end
 
-  # POST /:sub_domain/users
-  # POST /:sub_domain/users.json
-  def create
-    @user = User.new(params[:user])
-    flash[:notice] = "User was created successfully." if @user.save
-    respond_with @user, location: lodge_users_path(current_lodge.sub_domain)
-  end
-
-  # PATCH/PUT /:sub_domain/users/1
-  # PATCH/PUT /:sub_domain/users/1.json
+  # PATCH/PUT /u/profile/1
+  # PATCH/PUT /u/profile/1.json
   def update
-    flash[:notice] = "User was successfully updated." if @user.update(params[:user])
-    respond_with @user, location: lodge_users_path(current_lodge.sub_domain)
-  end
-
-  # DELETE /:sub_domain/users/1
-  # DELETE /:sub_domain/users/1.json
-  def destroy
-    # if the user is a plural member,
-    # only delete the association
-    if @user.memberships.count > 1
-      @user.memberships.find_by_lodge_id(current_lodge).destroy
-    else
-      @user.destroy
+    if @user.update_with_password(params[:user])
+      # Sign in the user by passing validation in case his password changed
+      sign_in @user, :bypass => true if @user.id == current_user.id
+      flash[:notice] = "User was successfully updated."
     end
-    respond_with @user, location: lodge_users_path(current_lodge.sub_domain), notice: 'User was successfully removed.'
+    respond_with @user, location: user_path
   end
 
   private
 
-  def verify_deletability
-    if current_user.id == params[:id].to_i
-      return redirect_to lodge_users_path(current_lodge.sub_domain), error: "Opps. You can't delete yourself."
-    end
-    raise ApplicationController::Unauthorized unless current_user.can_admin?(current_lodge)
-  end
-
-  def verify_editability
-    unless current_user.id == params[:id].to_i || current_user.can_admin?(current_lodge)
-      raise ApplicationController::Unauthorized
-    end
-  end
-
-  def verify_lodge_ability
-    raise ApplicationController::Unauthorized unless current_user.can_admin?(current_lodge)
-  end
-
   # Use callbacks to share common setup or constraints between actions.
   def set_user
-    # FIXME
-    #   - might want to scope this down to the current_lodge
-    #   - if so, be sure to rescue from potental ActiveRecord::RecordNotFound
-    # @user = current_lodge.members.find(params[:id])
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
 end
--- spec/routing/accounts/users_routing_spec.rb
+++ spec/routing/accounts/users_routing_spec.rb
@@ -3,36 +3,16 @@ require "spec_helper"
 describe Accounts::UsersController do
   describe "routing" do
 
-    it "routes to #dashboard" do
-      expect(:get => "/u/dashboard").to route_to("accounts/users#dashboard")
-    end
-
-    it "routes to #index" do
-      expect(:get => "/lodge109/users").to route_to("accounts/users#index", sub_domain: "lodge109")
-    end
-
-    it "routes to #new" do
-      expect(:get => "/lodge109/users/new").to route_to("accounts/users#new", sub_domain: "lodge109")
-    end
-
     it "routes to #show" do
-      expect(:get => "/lodge109/users/1").to route_to("accounts/users#show", sub_domain: "lodge109", id: "1")
+      expect(:get => "/u/profile").to route_to("accounts/users#show")
     end
 
     it "routes to #edit" do
-      expect(:get => "/lodge109/users/1/edit").to route_to("accounts/users#edit", sub_domain: "lodge109", id: "1")
-    end
-
-    it "routes to #create" do
-      expect(:post => "/lodge109/users").to route_to("accounts/users#create", sub_domain: "lodge109")
+      expect(:get => "/u/profile/edit").to route_to("accounts/users#edit")
     end
 
     it "routes to #update" do
-      expect(:put => "/lodge109/users/1").to route_to("accounts/users#update", sub_domain: "lodge109", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/lodge109/users/1").to route_to("accounts/users#destroy", sub_domain: "lodge109", id: "1")
+      expect(:put => "/u/profile").to route_to("accounts/users#update")
     end
 
   end
